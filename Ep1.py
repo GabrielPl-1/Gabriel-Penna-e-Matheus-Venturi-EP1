b0b7a5dbe16b124a09f3e8f2c05d097b60ca6a9c
Matheus Venturi <matheusmv@al.insper.edu.br>
2020-04-03 12:33:08 -0300

    while

--- Ep1.py
+++ Ep1.py
@@ -4,3 +4,15 @@ dado2 = random.randint(1,6)
 soma = dado1 + dado2 
 a = input('Você quer apostar ? ')
 fichas = 100
+while fichas>0:
+    if a == 'sim':
+        b = input('que tipo de apostar voce quer fazer?')
+        if b=='line bet':
+            c=int(input('Quantas fichas voce quer apostar ?'))
+            fichas= (fichas - c)
+            if soma==7 or soma==11:
+                print ('voce ganhou ' + str(c) + ' fichas')
+                fichas = fichas+(2*c)
+            elif soma==2 or soma==3 or soma==12:
+                print ('voce perdeu '+ str(c) +' fichas')
+                fichas = fichas - c
