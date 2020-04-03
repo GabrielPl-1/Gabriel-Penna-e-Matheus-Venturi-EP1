126ff7542c7cab81208cb637f68659c00d21595e
Gabriel Penna de Lima <gabrielpl@al.insper.edu.br>
2020-04-03 12:36:58 -0300

    Fase points

--- Ep1.py
+++ Ep1.py
@@ -16,3 +16,44 @@ while fichas>0:
             elif soma==2 or soma==3 or soma==12:
                 print ('voce perdeu '+ str(c) +' fichas')
                 fichas = fichas - c
+            else:
+                print('você está entrando na fase points')
+                dado3= random.randint(1,6)
+                dado4=random.randint(1,6)
+                soma2=dado3+dado4
+                if soma2==soma:
+                    print('você ganhou'+ str(2*c)+ 'fichas')
+                    fichas = fichas +(2*c)
+                elif soma2==7:
+                    print ('você perdeu'+ str(c)+ 'ficha')
+                    fichas=fichas-c
+                else: 
+                    print('você vai permanecer na fase points')
+                    x=input('Que tipo de aposta você quer fazer ? ')
+                    if x=='fields':
+                        h=int(input('Quanto você quer apostar ? '))
+                        fichas = fichas - h
+                        if soma2==2:
+                            print('Você ganhou' + str(3*h) +'fichas')
+                            fichas= fichas+(3*h)
+                        elif soma==12:
+                            print ('você ganhou' + str(4*h)+ 'fichas')
+                        elif soma==5 or soma==6 or soma==7 or soma==8:
+                            print ('você perdeu' + str(h)+ 'fichas')
+                        else:
+                            print ('você ganhou '+ str(2*h)+ 'fichas')
+                            fichas= fichas+(2*h)
+                    elif x=='any crops':
+                        j=int(input('quanto você quer apostar ? '))
+                        fichas = fichas - j
+                        if soma2==2 or soma2==3 or soma2==7:
+                            print('você ganhou '+ str(7*j)+ 'fichas')
+                            fichas= fichas + (7*j)
+                        else:
+                            print('você perdeu '+ str(j)+ 'fichas')
+                    elif x=='twelve':
+                        r=int(input('Quanto você quer apostar ?  '))
+                        if soma2==12:
+                            print ('Você ganhou' + str(12*r)+ 'fichas')
+                        else:
+                            print ('Você perdeu' + str(r)+ 'fichas')
